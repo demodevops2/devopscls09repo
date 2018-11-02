abd9d519cee6d57e60d381f9df472eee7d7476cf
demodevops2 <ganeshcloud007@gmail.com>
2018-11-02 18:35:54 +0530

    update gkfile

--- gkfile.sh
+++ gkfile.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-cat /etc/*se
+sudo apt-get update
+sudo apt-get install apache2 -y
+sudo /etc/init.d/apache2 start
+sudo netstst -ntulp
+sudo echo "Welcome to jenkins" > /var/www/html/index.html
+sudo /etc/init.d/apache2 restart
 
 
