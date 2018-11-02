6112e15a7523f25684803c785ea4f23e05a9a865
demodevops2 <ganeshcloud007@gmail.com>
2018-11-02 18:58:42 +0530

    update gkfile

--- gkfile.sh
+++ gkfile.sh
@@ -3,7 +3,7 @@ sudo apt-get update
 sudo apt-get install apache2 -y
 sudo /etc/init.d/apache2 start
 sudo netstst -ntulp
-sudo echo "Welcome to jenkins" > /var/www/html/index.html
+sudo echo "Welcome to jenkins and cheking webhooks auto trigger" > /var/www/html/index.html
 sudo /etc/init.d/apache2 restart
 
 
