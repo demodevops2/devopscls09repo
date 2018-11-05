dda5c0e7728f72f275eb4a1b811761473e2aa6d1
demodevops2 <31069933+demodevops2@users.noreply.github.com>
2018-11-05 23:45:01 +0530

    chk1.0

--- gkfile.sh
+++ gkfile.sh
@@ -1,5 +1,5 @@
 version = 1.1
-url="http://35.165.247.100:8081/nexus/service/local/artifact/maven/redirect?r=maven-releases&g=doc-examples&a=lambda-java-example&v=$version&e=zip"
+url="http://35.165.247.100:8081/service/local/artifact/maven/redirect?r=maven-releases&g=doc-examples&a=lambda-java-example&v=$version&e=zip"
   echo "#"
   echo "# Try to fetch from Snapshot repo for version: " $version " from " $url
   echo "#"
