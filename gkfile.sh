a4a8c3cb6ef2661b61d3e447457398484f42dbe5
demodevops2 <ganeshcloud007@gmail.com>
2018-11-07 16:29:40 +0530

    yml

--- gkfile.sh
+++
@@ -1,7 +0,0 @@
-version = 1.2
-url="http://35.165.247.100:8081/nexus/service/local/artifact/maven/redirect?r=maven-releases&g=doc-examples&a=lambda-java-example&v=$version&e=zip"
-  echo "#"
-  echo "# Try to fetch from Snapshot repo for version: " $version " from " $url
-  echo "#"
-  WGET=`/usr/wget $url --output-document=$releasefile`
-  EXITCODE=$?
